865bfb50a433e509e975c6a134037e6b4d1684e1
su-kino <97873142+su-kino@users.noreply.github.com>
2022-06-21 15:44:36 +0900

    [Chapter1]~execise1_3

---
+++ src/exercises/Exercise1_2.java
@@ -0,0 +1,10 @@
+package exercises;
+
+public class Exercise1_2 {
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 5;
+		int c = a * b;
+		System.out.println("縦幅3横幅5の長方形の面積は、" + c);
+	}
+}
---
+++ src/exercises/Exercise1_3.java
@@ -0,0 +1,9 @@
+package exercises;
+
+public class Exercise1_3 {
+	boolean result = true;
+	char character = '駆';
+	double pi = 3.14;
+	long num = 314159265853979L;
+	String str = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+}
