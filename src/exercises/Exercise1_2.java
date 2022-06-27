b4a7734875f04543bbdd670b1996fcb3b54459a9
su-kino <97873142+su-kino@users.noreply.github.com>
2022-06-27 15:41:16 +0900

    [Chapter3]~execise3_6

--- src/exercises/Exercise1_2.java
+++ src/exercises/Exercise1_2.java
@@ -1,10 +1,21 @@
 package exercises;
 
+import java.io.*;
+
 public class Exercise1_2 {
-	public static void main(String[] args) {
-		int a = 3;
-		int b = 5;
-		int c = a * b;
-		System.out.println("縦幅3横幅5の長方形の面積は、" + c);
+	public static void main(String[] args) throws IOException {
+//		int a = 3;
+//		int b = 5;
+//		int c = a * b;
+//		System.out.println("縦幅3横幅5の長方形の面積は、" + c);
+		
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int x = Integer.parseInt(br.readLine());
+		int y = Integer.parseInt(br.readLine());
+		
+		if (x > y) {
+			System.out.println("xはyより大きい。");
+		}
 	}
 }
---
+++ src/exercises/Exercise3_3.java
@@ -0,0 +1,22 @@
+package exercises;
+
+public class Exercise3_3 {
+	public static void main(String[] args) {
+		int isHungry = 0;
+		String food = "りんご";
+		
+		System.out.println("こんにちは");
+		
+		if (isHungry == 0) {
+			System.out.println("お腹がいっぱいです");
+		} else {
+			System.out.println("はらぺこです");
+		}
+		
+		if (isHungry == 1) {
+			System.out.println(food + "をいただきます");
+		}
+		
+		System.out.println("ごちそうさまでした");
+	}
+}
