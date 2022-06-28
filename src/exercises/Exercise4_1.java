eb17be43b029f929f744c51894aa553bcb9f30df
su-kino <97873142+su-kino@users.noreply.github.com>
2022-06-28 20:44:30 +0900

    [Chapter4]~execise4_4

---
+++ src/exercises/Exercise4_1.java
@@ -0,0 +1,10 @@
+package exercises;
+
+public class Exercise4_1 {
+	public static void main(String[] args) {
+		int[] points = new int[4];
+		double[] weights = new double[5];
+		boolean [] amswers = new boolean[3];
+		String[] names = new String[3];
+	}
+}
